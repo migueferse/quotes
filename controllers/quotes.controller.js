21795c5191f8e4c57d3c8298e4c95a4c5b205fbc
migueferse <migueferse@gmail.com>
2020-09-21 23:02:27 +0200

    End

--- controllers/quotes.controller.js
+++ controllers/quotes.controller.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const DB_PATH = path.join(__dirname + '/../data/db.json');
+let db = require(DB_PATH);
+const fs = require('fs');
 
 function render(file, res) {
     return res.sendFile(path.join(__dirname + `/../views/${file}.html`));
@@ -7,6 +10,18 @@ function render(file, res) {
 class QuotesController {
     async index(req, res) {
         return render('quotes', res);
+    }
+    async get(req, res) {
+        return res.send(db);
+    }
+    async add(req, res) {
+        const {body: quote} = req;
+        const lastQuote = db[db.length - 1];
+        const {id} = lastQuote;
+        quote.id = id + 1;
+        db.push(quote);
+        fs.writeFileSync(DB_PATH, JSON.stringify(db));
+        return res.status(201).send();
     }
 }
 
